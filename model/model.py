eaaaff00f7afe4dcd44a10b93186cb734c7da7c7
sgitcoding <147993332+sgitcoding@users.noreply.github.com>
2024-06-07 11:53:47 +0530

    Update model.py

--- model/model.py
+++ model/model.py
@@ -20,57 +20,13 @@ from keras.layers import Dense
 
 import tensorflow as tf
 
-
-
-import keras_tuner 
-from keras_tuner.tuners import RandomSearch
-
 #to test the model 
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import precision_score , recall_score , accuracy_score , f1_score
 
 df = pd.read_csv('heart_disease_health_indicators_BRFSS2015.csv')
 
-#print the first 5 data samples 
 
-df.head()
-
-#print the last 5 data samples 
-
-df.tail()
-
-df.isnull().sum()
-
-df.dtypes
-
-sns.histplot(df['Age'])
-
-df.columns
-col = ['HighBP', 'HighChol', 'CholCheck',
-       'Smoker', 'Stroke', 'Diabetes', 'PhysActivity', 'Fruits', 'Veggies',
-       'HvyAlcoholConsump', 'AnyHealthcare', 'NoDocbcCost', 'GenHlth',
-       'DiffWalk', 'Sex', 'Education',
-       'Income']
-plt.figure(figsize=(5,30))
-for i,column in enumerate(col):
-    plt.subplot(len(col), 2, i+1)
-    plt.suptitle("Countplot of Categories", fontsize=15, x=0.5, y=1)
-    sns.countplot(data=df, x=column)
-    plt.title(f"{column}")
-    plt.tight_layout()
-
-hd = (df["HeartDiseaseorAttack"] == 1.0).sum()
-nohd = (df["HeartDiseaseorAttack"] == 0.0).sum()
-
-sizes = [hd, nohd]
-labels = ['Heart Disease', 'No Heart Disease']
-
-plt.figure(figsize=(6, 4))
-plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140)
-plt.title('Heart Disease Rate')
-
-plt.axis('equal')
-plt.show()
 
 x = df.drop('HeartDiseaseorAttack', axis =1)
 y = df['HeartDiseaseorAttack']
